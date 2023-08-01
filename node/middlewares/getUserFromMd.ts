9aae4a0c111f315b99af36c085172b6c01b47b15
Bianca Santos <luisabianca93@gmail.com>
2023-08-01 00:04:51 -0300

    finish class 2 of seconde section

--- node/middlewares/getUserFromMd.ts
+++ node/middlewares/getUserFromMd.ts
@@ -1,13 +1,21 @@
 export async function getUserFromMd(ctx: Context, next: () => Promise<void>) {
-
+  ctx.set('Cache-Control', 'no-cache');
   const { email } = ctx.query;
 
-  console.log(email, "email");
+  const response = await ctx.clients.masterdata.searchDocuments({
+    dataEntity: 'CL',
+    fields: ['documents', 'email', 'firstName'],
+    pagination: {
+      page: 1,
+      pageSize: 1
+    },
+    where: `email=${email}` 
+  })
 
   ctx.body = {
-    email
+    response
   };
 
   ctx.status = 200;
-  await next();
+ next();
 }
